f2535c71ee4161bf68c5c5ba71bbb3780d12af23
KirstyFurey <87201710+KirstyFurey@users.noreply.github.com>
2024-03-13 14:35:19 +0000

    add js css for nav modal

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,2 +1,32 @@
 // JavaScript Document
-	
+// GET BUTTON TO OPEN MODAL
+const btn = document.getElementById('btn');	
+// GET MODAL
+const modal = document.getElementById('jump-links');
+// GET CLOSE BUTTON
+const close = document.getElementsByClassName('close');
+
+// SHOW MODAL
+const openModal = () => {
+	modal.style.display = 'block';
+}
+
+//CLOSE MODAL
+const closeModal = () => {
+	modal.style.display = 'none';
+}
+
+// CLICK TO OPEN
+btn.addEventListener('click', openModal);
+// CLICK CLOSE
+
+[...close].forEach((close) => {
+    close.onclick = () => (modal.style.display = 'none');
+});
+//close.addEventListener('click', closeModal);
+
+
+
+//function openModal(){
+  //modal.style.display = 'block';
+//}
