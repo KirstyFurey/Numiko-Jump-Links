2d29e716cc0af6efd2d01f79b851515761167afe
KirstyFurey <87201710+KirstyFurey@users.noreply.github.com>
2024-03-15 17:04:36 +0000

    fix issue with scroll bar progress

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,30 +1,30 @@
 // JavaScript Document
-// GET BUTTON TO OPEN MODAL
+// GET BUTTON TO OPEN NAV MODAL
 const btn = document.getElementById('btn');	
-// GET MODAL
+// GET NAV MODAL
 const modal = document.getElementById('jump-links');
 // GET MULTIPLE CLOSE BUTTONS
 const close = document.getElementsByClassName('close');
 
-// SHOW MODAL
+// SHOW NAV MODAL
 const openModal = () => {
 	modal.style.display = 'block';
 }
 
-//CLOSE MODAL
+//CLOSE NAV MODAL
 const closeModal = () => {
 	modal.style.display = 'none';
 }
 
 // CLICK TO OPEN
 btn.addEventListener('click', openModal);
-// CLICK CLOSE ON MULTIPLE LOCATIONS - ANY OTSIDE OF THE NAV OR MODAL
+// CLICK CLOSE ON MULTIPLE LOCATIONS - ANY OUTSIDE OF THE NAV OR NAV MODAL
 [...close].forEach((close) => {
     close.onclick = () => (modal.style.display = 'none');
 });
 
 //GET JUMP LINKS SECTION
-const main = document.querySelector('#main');
+const main = document.querySelector('main');
 // GET SECTION AFTER JUMP LINKS
 const end = document.querySelector('#end-stick');
 // WHEN MAIN SECTION REACHES TOP
@@ -32,24 +32,17 @@ const mainTop = main.offsetTop;
 // WHEN SECTION AFTER JUMP LINKS REACHES TOP
 const second = end.offsetTop;
 
- 
-const progress = () => {
-	let scroll = document.documentElement.scrollTop;
-	let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-	let scrollProgress = (scroll / height) * 100;
-	document.getElementById("scroll-progress").style.width = scrollProgress + "%";
-}
-
 // IF THE NAV REACHES THE TOP OF THE SCREEN, KEEP IT THERE UNTIL IT ENTERS THE SECTION AFTER THE JUMP LINKS
+// ADD SCROLL BAR BROGRESS
 const makeStick = () => {
 	if (window.scrollY >= mainTop && window.scrollY <= second) {
 		document.body.classList.add('sticky')
-        progress('#main');
 	} else  {
 		document.body.classList.remove('sticky');
 	}
 }
 
+//VARIABLES FOR TEXT CHANGE
 let one = document.querySelector('#one-txt');
 let two = document.querySelector('#two');
 let three = document.querySelector('#three');
@@ -60,6 +53,7 @@ let threeTop = three.offsetTop;
 let fourTop = four.offsetTop;
 let pCont = document.getElementById('text');
 
+// FUNCTION TO CHANGE NAV SECTION TEXT
 const section = () => {
 	if (window.scrollY >= oneTop && window.scrollY <= twoTop) {
 		pCont.innerText = "First Things Second";
@@ -73,6 +67,28 @@ const section = () => {
 		pCont.innerText = "First Thing's First";
 	}
 }
+// NON WORKING
+const scrollProgress = document.getElementById('scroll-progress')
+
+const progress = () => {
+    const scroll = main.scrollTop;
+    const height = main.scrollHeight - main.clientHeight;
+    const percent = (scroll / height) * 100;
+    scrollProgress.style.width = percent + "%";
+}
+/*
+//SCROLL PROGRESS FOR WHOLE PAGE. NOT RIGHT BUT SHOWS PROGRESS BAR FILLING.
+const progress = () => {
+	let scroll = document.documentElement.scrollTop;
+	let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+	let scrollProgress = (scroll / height) * 100;
+	document.getElementById("scroll-progress").style.width = scrollProgress + "%";
+}*/
+
+main.addEventListener('scroll', progress);
+
+// RUN TEXT FUNCTION ON SCROLL
 window.addEventListener('scroll', section);
 
+// RUN FUNCTION TO STICK AND UNSTICK NAV ON SCROLL
 window.addEventListener('scroll', makeStick);
